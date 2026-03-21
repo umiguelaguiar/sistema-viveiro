c025cb232ee0c7fdcd2ec4c69daa38b788a61ac6
umiguelaguiar <umiguelaguiar@gmail.com>
2026-03-21 15:13:32 +0000

    File changes

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -231,6 +231,9 @@ export default function Admin() {
         ))}
       </div>
 
+      {/* Solicitações de acesso */}
+      <SolicitacoesSection />
+
       {/* Permissions reference */}
       <Card className="mb-6">
         <CardHeader className="pb-3">
